a5519f3f266d5c0d1eb201acf052cbe1deb3686d
fatrique <lar.patrik@gmail.com>
2019-05-02 18:05:03 +0200

    Fixed small bugs in query file

--- dbcreation.sql
+++ dbcreation.sql
@@ -1,8 +1,8 @@
 CREATE SCHEMA `global` ;
 CREATE TABLE `global`.`registered_users` (
-  `email` INT NOT NULL,
+  `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `email` VARCHAR(45) NOT NULL UNIQUE,
   `firstname` VARCHAR(45) NOT NULL,
   `lastname` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`email`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+  `password` VARCHAR(45) NOT NULL
+);
