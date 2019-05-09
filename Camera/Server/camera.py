318be23a024b3d38d8bc16d821d22a8f6034dd68
DungKaKa <hdbluetc@gmail.com>
2019-05-09 21:18:51 +0700

    Implement

--- Camera/Server/camera.py
+++ Camera/Server/camera.py
@@ -3,36 +3,22 @@ import socket
 
 class VideoCamera(object):
     def __init__(self, ip, port):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
-        print("\nINIT HERE\n")
+        # Using OpenCV to capture from device 0: VideoCapture(0), example webcam.
+        # For IP Camera (From device like smartphone, VideoCapture("IP address: port")
         self.checkIP(ip, int(port, 10))
         self.video = cv2.VideoCapture("http://" + ip + ":" + port + "/video")
-        print(self.video)
-
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
-        # self.video = cv2.VideoCapture('video.mp4')
 
     def __del__(self):
-        print("\n DELETED HERE \n")
-
         if hasattr(self, 'video'):
             self.video.release()
-            cv2.waitKey(500)
             cv2.destroyAllWindows()
-            cv2.waitKey(500)
 
     def checkIP(self, ip, port):
         s = socket.socket()
         s.settimeout(3)
         try:
-            print("BEFORE CONNECT")
             s.connect((ip, port))
-            print("AFTER CONNECT")
             s.close()
-            print("CLOSE SUCCESS")
         except:
            raise AssertionError("WRONG IP")
 
@@ -40,9 +26,6 @@ class VideoCamera(object):
             success, image = self.video.read()
             (w, h, c) = image.shape
             image = cv2.resize(image,(600, 400))
-            print("\n")
-            print(success)
-            print("\n")
 
             # We are using Motion JPEG, but OpenCV defaults to capture raw images,
             # so we must encode it into JPEG in order to correctly display the
--- Camera/Server/main.py
+++ Camera/Server/main.py
@@ -1,18 +1,3 @@
-#!/usr/bin/env python
-#
-# Project: Video Streaming with Flask
-# Author: Log0 <im [dot] ckieric [at] gmail [dot] com>
-# Date: 2014/12/21
-# Website: http://www.chioka.in/
-# Description:
-# Modified to support streaming out with webcams, and not just raw JPEGs.
-# Most of the code credits to Miguel Grinberg, except that I made a small tweak. Thanks!
-# Credits: http://blog.miguelgrinberg.com/post/video-streaming-with-flask
-#
-# Usage:
-# 1. Install Python dependencies: cv2, flask. (wish that pip install works like a charm)
-# 2. Run "python main.py".
-# 3. Navigate the browser to the local webpage.
 from flask import Flask, render_template, Response, request
 from camera import VideoCamera
 from flask_cors import CORS
@@ -23,42 +8,32 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
 def gen(camera):
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-    
+
     camera.video.release()
-    cv2.waitKey(500)
     cv2.destroyAllWindows()
-    cv2.waitKey(500)
 
 
 @app.route('/video_feed')
 def video_feed():
     id = request.args.get("id")
     ip, port = id.split(':')
-    print("PRINT ID", ip, int(port, 10))
     try:
-        print("CREATE OBJECT CAMERA")
         camera = VideoCamera(ip, port)
-        print("CREATE SUCCESS")
-        response = Response(gen(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+        response = Response(
+            gen(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
         return response
     except:
-        print("EXCEPT HERE")
         img = cv2.imread('novideo.jpg')
         data = cv2.imencode('.jpg', img)[1].tobytes()
         return Response(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n'
-             + data 
-             + b'\r\n\r\n', 
-            mimetype='multipart/x-mixed-replace; boundary=frame')
+                        + data
+                        + b'\r\n\r\n',
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
